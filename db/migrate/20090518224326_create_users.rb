6cdb1cad01d459df45ba5a14de405acb7b231408
Cameron Adamez <camo@bjartur.local>
2009-05-27 16:46:40 -0700

    Added fields to "create users"

--- db/migrate/20090518224326_create_users.rb
+++ db/migrate/20090518224326_create_users.rb
@@ -1,8 +1,19 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-
-      t.timestamps
+    
+      t.string :firstname
+      t.string :lastname 
+      t.string :wholename 
+      t.string :email
+      t.string :url # home page, blog, whatever
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zipcode
+      t.string :geo # latitude and longitude
+      t.string :photo # This will be in /public/photos or something 
+      t.bool :biz, :default => false # A check if something is a business 
     end
   end
 
