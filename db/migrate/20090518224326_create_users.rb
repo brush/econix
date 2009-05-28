f1d6c462ec4e09c53793f6a7c4b97055e10bccf2
Cameron Adamez <camo@bjartur.local>
2009-05-27 22:01:25 -0700

    Fixed strange merge problems, added things to Users to be compatible with hCards

--- db/migrate/20090518224326_create_users.rb
+++ db/migrate/20090518224326_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-    
       t.string :firstname
       t.string :lastname 
       t.string :wholename 
@@ -14,6 +13,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :geo # latitude and longitude
       t.string :photo # This will be in /public/photos or something 
       t.bool :biz, :default => false # A check if something is a business 
+     end
     end
   end
 
